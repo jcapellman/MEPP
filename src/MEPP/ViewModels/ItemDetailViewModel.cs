607edf6a50ae8fcc1891555afee17655b8b04e5a
Jarred Capellman <jcapellman@hotmail.com>
2019-10-03 19:15:28 -0500

    Added: Initial commit

---
+++ src/MEPP/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MEPP.Models;
+
+namespace MEPP.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
